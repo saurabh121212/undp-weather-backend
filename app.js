41f8e574e71b7cd2affe24bf8f65f8f1d0b8b6d3
Saurabh <saurabhsaini38@gmail.com>
2025-04-26 11:58:29 +0530

    update

--- app.js
+++ app.js
@@ -13,6 +13,11 @@ global._config = require('./config/config.js');
 console.log('Environment:', _config.app);
 
 app.use(cors());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
